1d5fd30cad950120c2c197d85311ed3fb87b154a
Seedling <david.r.graham32@gmail.com>
2025-02-17 11:41:11 -0500

    Header and footer

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { APP_NAME } from '@/lib/constants';
+
+const Header = () => {
+  return (
+    <header className="bg-slate-800 w-full">
+      <div className="wrapper flex-between py-5">
+        <div className="flex-start">
+          <Link href="/" className="flex-start">
+            <Image
+              src="/images/logo.svg"
+              alt={`${APP_NAME} logo.`}
+              height={48}
+              width={48}
+              priority={true}
+              className="rounded-lg"
+            />
+            <span className="hidden lg:block font-bold text-2xl ml-3 text-white">
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
