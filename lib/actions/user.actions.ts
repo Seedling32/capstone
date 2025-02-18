e13a442fcb87bc5ee2959e1a22c2a48984e895fe
Seedling <david.r.graham32@gmail.com>
2025-02-18 11:00:30 -0500

    Create customized error messages. Create utility function that gets and returns custom error messages based on zod or prisma. Add the function to user action when throwing error.

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { signIn, signOut } from '@/auth';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
 import { hashSync } from 'bcrypt-ts-edge';
 import { prisma } from '@/db/prisma';
+import { formatError } from '../utils';
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -68,14 +69,10 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
       message: 'User registered successfully.',
     };
   } catch (error) {
-    console.log(error.name);
-    console.log(error.code);
-    console.log(error.errors);
-    console.log(error.meta?.target);
     if (isRedirectError(error)) {
       throw error;
     }
 
-    return { success: false, message: 'User unable to be registered.' };
+    return { success: false, message: formatError(error) };
   }
 }
