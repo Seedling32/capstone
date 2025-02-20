8fa83e9519da7230cd32c9036ab12c20397b1bf9
Seedling <david.r.graham32@gmail.com>
2025-02-20 17:13:54 -0500

    CHange layout to be side by side.

--- app/(root)/rides/[slug]/page.tsx
+++ app/(root)/rides/[slug]/page.tsx
@@ -1,7 +1,9 @@
 import { getRideBySlug } from '@/lib/actions/ride.actions';
 import DynamicMap from '@/components/shared/map/DynamicMap';
 
-const RideDetailsPage = async (props: { params: Promise<{ slug: string }> }) => {
+const RideDetailsPage = async (props: {
+  params: Promise<{ slug: string }>;
+}) => {
   const { slug } = await props.params;
 
   const ride = await getRideBySlug(slug);
@@ -11,13 +13,18 @@ const RideDetailsPage = async (props: { params: Promise<{ slug: string }> }) => 
   }
 
   // Parse path from string to array of objects if needed
-  const parsedPath = typeof ride.path === 'string' ? JSON.parse(ride.path) : ride.path;
+  const parsedPath =
+    typeof ride.path === 'string' ? JSON.parse(ride.path) : ride.path;
 
   return (
-    <div className="container mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-4">{ride.shortDescription}</h1>
-      <p className="text-lg mb-4">{ride.longDescription}</p>
-      <DynamicMap path={parsedPath} zoom={15} />
+    <div className="container flex justify-self-center justify-between  gap-10 mt-8 mx-4">
+      <div>
+        <h1 className="text-3xl font-bold mb-4">{ride.shortDescription}</h1>
+        <p className="text-lg mb-4">{ride.longDescription}</p>
+      </div>
+      <div className="container mx-auto p-6">
+        <DynamicMap path={parsedPath} zoom={15} />
+      </div>
     </div>
   );
 };
