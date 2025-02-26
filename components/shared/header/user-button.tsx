c61ab3af1f543b2f198e88acf5a2de4dc3f7d958
Seedling <david.r.graham32@gmail.com>
2025-02-26 14:44:05 -0500

    Add user pages to user button drop down list

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -59,6 +59,16 @@ const UserButton = async () => {
               </div>
             </div>
           </DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Link href="/user/profile" className="w-full">
+              User Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/user/my-rides" className="w-full">
+              My Rides
+            </Link>
+          </DropdownMenuItem>
           <DropdownMenuItem className="p-0 mb-1">
             <form action={signOutUser} className="w-full">
               <Button
