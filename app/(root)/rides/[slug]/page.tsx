236920237cd4abd0bc28c1d47bae81d6c1ffc065
Seedling <david.r.graham32@gmail.com>
2025-03-03 14:59:30 -0500

    Move create ride page. Edit static map url. Static map was saving a huge long list of lat/lng points as the url, use google polyline to encode the url to be shorter so that long rides don't exceed the url parameter.

--- app/(root)/rides/[slug]/page.tsx
+++ app/(root)/rides/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { getRideBySlug } from '@/lib/actions/ride.actions';
 import DynamicMap from '@/components/shared/map/DynamicMap';
 import SignUpForRide from '@/components/shared/rides/signup-for-ride';
 import { auth } from '@/auth';
+import { formatDateTime } from '@/lib/utils';
 
 const RideDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -22,10 +23,14 @@ const RideDetailsPage = async (props: {
     typeof ride.path === 'string' ? JSON.parse(ride.path) : ride.path;
 
   return (
-    <div className="container flex justify-self-center justify-between  gap-10 mt-8 mx-4">
-      <div>
+    <div className="container flex flex-col justify-self-center justify-between  gap-10 mt-8 mx-4 md:flex-row">
+      <div className="max-w-[300px]">
         <h1 className="text-3xl font-bold mb-4">{ride.shortDescription}</h1>
         <p className="text-lg mb-4">{ride.longDescription}</p>
+        <ul className="mb-6">
+          <li>Distance: {ride.distance} Miles</li>
+          <li>Date: {formatDateTime(ride.date).dateTime}</li>
+        </ul>
         {session && (
           <SignUpForRide
             ride={{
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -4,6 +4,9 @@ export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'A community-driven app for sharing bike routes, organizing group rides, and connecting cyclists in the Asheville, NC area.';
 
+export const GOOGLE_MAPS_API_KEY =
+  process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '';
+
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
 
