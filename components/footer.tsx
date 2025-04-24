089dca3663937d935149d0bf741b785423509b47
Seedling <david.r.graham32@gmail.com>
2025-04-24 14:31:05 -0400

    Add logo to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,19 @@
 import { APP_NAME } from '@/lib/constants';
+import Image from 'next/image';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-muted border-t">
-      <div className="p-5 flex-center">
-        {currentYear} {APP_NAME}. All Rights Reserved
+      <div className="p-5 flex items-center justify-center gap-2 text-center">
+        {currentYear} {APP_NAME}{' '}
+        <Image
+          src="/images/logo.png"
+          width={48}
+          height={48}
+          alt={`${APP_NAME} logo`}
+        />{' '}
+        All Rights Reserved
       </div>
     </footer>
   );
